a975984971ea158f88c0850e86b32e81c4720a28
Alex McBride <xandermcbride@gmail.com>
2018-12-05 15:25:28 +0000

    Added comments

--- scripts/content-loader.js
+++ scripts/content-loader.js
@@ -1,24 +1,25 @@
-//a content loader module that loads the appropriate content and passes control over
-// to the appropriate display routine eg the slide manager; the page updater etc
-
+// Responsible for loading content. Watches for hash changes and loads the page.
 var contentLoader = (function () {
     var seperator = '/';
     var mainContentEl = null;
     var pageMap = [];
     var defaultPageRoute = null;
 
+    // Adds page to the map for particular route.
     var addPage = function (route, page) {
         pageMap[route] = page;
     }
 
+    // Gets a handler for a specific route from the map.
     var getPageHandler = function (route) {
         if (pageMap[route] === undefined) {
-            return page.notFound; // :(
+            return page.notFound;
         } else {
             return pageMap[route];
         }
     }
 
+    // Loads the specified route, and attempts to load the content from that route if present.
     var load = function (route, contentId) {
         var pageHandler = getPageHandler(route);
         if (contentId !== undefined && contentId !== 0) {
@@ -35,10 +36,12 @@ var contentLoader = (function () {
         }
     }
 
+    // Renders the HTML to the main content element.
     var render = function (html) {
         document.getElementById(mainContentEl).innerHTML = html;
     }
 
+    // Called when the hash location changes, tries to parse the content out and calls load.
     var locationChanged = function () {
         var page = window.location.hash.substring(1);
         if (page.indexOf(seperator) > -1) {
@@ -51,6 +54,7 @@ var contentLoader = (function () {
         load(page, contentId);
     }
 
+    // Gets the content ID portion of a hash or blank string.
     var getContentId = function () {
         if (window.location.hash) {
             var hash = window.location.hash.substring(1);
@@ -58,9 +62,10 @@ var contentLoader = (function () {
                 return hash.split(seperator)[1];
             }
         }
-        return 0;
+        return '';
     }
 
+    // Counts the number of seperators in the specified string.
     var countSeperators = function (str) {
         var count = 0;
         for (var i = 0; i < str.length; i++) {
@@ -71,6 +76,7 @@ var contentLoader = (function () {
         return count;
     }
 
+    // Gets the data portion of a hash, which is bit after the last seperator.
     var getData = function () {
         var hash = window.location.hash;
         if (hash) {
@@ -89,6 +95,7 @@ var contentLoader = (function () {
         return 0;
     }
 
+    // Creates a URL for the specified page.
     var url = function (page, pageId, dataId) {
         var url = page;
         if (pageId) {
@@ -100,6 +107,7 @@ var contentLoader = (function () {
         return url;
     }
 
+    // Runs the content loader, which draws the main content, and watches for location changes.
     var run = function (defaultPage, contentEl) {
         defaultPageRoute = defaultPage;
         mainContentEl = contentEl;
