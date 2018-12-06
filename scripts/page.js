da2fbf03dda77d18b5ff429ea4950b225882ad38
Alex McBride <xandermcbride@gmail.com>
2018-12-06 12:49:31 +0000

    Added home styling

--- scripts/page.js
+++ scripts/page.js
@@ -2,7 +2,14 @@
 
 var page = (function () {
     var home = function () {
-        var html = '<h2>Home</h2><p>Intro to web site and stuff...</p>';
+        var html = '<div class="jumbotron">';
+        html += '<h1 class="display-4">Welcome</h1>';
+        html += '<p class="lead">Welcome to the interactive guide to the first world war. It is the centenary of the great war and this web site has been created to commemorate it.</p>';
+        html += '<hr class="my-4">';
+        html += '<p>Here you will find information about the war, slideshows, videos, and quizes. All designed to help you understand what happened and why.</p>';
+        html += '<a class="btn btn-primary btn-lg" href="#post/1" role="button">Start your journey!</a>';
+        html += '</div>';
+
         content.render(html);
     }
 
