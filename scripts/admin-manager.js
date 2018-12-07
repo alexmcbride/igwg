0099ada9f95314dc7844f264e84048606dabf78b
Alex McBride <xandermcbride@gmail.com>
2018-12-07 09:12:40 +0000

    Added image and video handler

--- scripts/admin-manager.js
+++ scripts/admin-manager.js
@@ -1,6 +1,15 @@
 var adminManager = (function () {
     var currentPage = null;
 
+    var validateRequired = function (id, name) {
+        var title = document.getElementById(id).value.trim();
+        if (title.length == 0) {
+            document.getElementById(id + '-error').innerHTML = name + ' is required';
+            return false;
+        }
+        return true;
+    }
+
     // Class to represent adding post.
     var Post = {
         form: function () {
@@ -21,6 +30,10 @@ var adminManager = (function () {
             document.getElementById('post-title').value = page.title;
             document.getElementById('post-content').value = page.content;
         },
+        clear: function () {
+            document.getElementById('post-title').value = '';
+            document.getElementById('post-content').value = '';
+        },
         save: function () {
             dataStore.setPage({
                 id: createId(),
@@ -29,21 +42,19 @@ var adminManager = (function () {
                 created: new Date().toISOString(),
                 content: this.getContent()
             });
-            document.getElementById('post-title').value = '';
-            document.getElementById('post-content').value = '';
+            this.clear();
         },
         validate: function () {
             var success = true;
-            var title = this.getTitle();
-            if (title.length == 0) {
-                document.getElementById('post-title-error').innerHTML = 'Title is required';
+
+            if (!validateRequired('post-title')) {
                 success = false;
             }
-            var content = this.getContent();
-            if (content.length == 0) {
-                document.getElementById('post-content-error').innerHTML = 'Content is required';
+
+            if (!validateRequired('post-content')) {
                 success = false;
             }
+
             return success;
         },
         getTitle: function () {
@@ -105,9 +116,8 @@ var adminManager = (function () {
         },
         validate: function () {
             var success = true;
-            var title = this.getTitle();
-            if (title.length == 0) {
-                document.getElementById('slideshow-title-error').innerHTML = 'Title is required';
+
+            if (!validateRequired('slideshow-title')) {
                 success = false;
             }
 
@@ -162,6 +172,171 @@ var adminManager = (function () {
         }
     };
 
+    // Required: form, update, save, validate
+    var Video = {
+        form: function () {
+            var html = '<div class="video-form">';
+            html += '<h3>Video</h3>';
+            html += '<div class="form-group">';
+            html += '<label for="video-title">Title</label><br>';
+            html += '<input type="text" id="video-title" class="form-control">';
+            html += '<span class="form-error" id="video-title-error"></span>';
+            html += '</div>';
+            html += '<div class="form-group">';
+            html += '<label for="video-src">Video URL</label><br>';
+            html += '<input type="text" id="video-src" class="form-control">';
+            html += '<span class="form-error" id="video-src-error"></span>';
+            html += '</div>';
+            html += '<div class="form-group">';
+            html += '<label for="video-type">Content-Type</label><br>';
+            html += '<input type="text" id="video-type" class="form-control">';
+            html += '<span class="form-error" id="video-type-error"></span>';
+            html += '</div>';
+            return html;
+        },
+        update: function (page) {
+            document.getElementById('video-title').value = page.title;
+            document.getElementById('video-src').value = page.src;
+            document.getElementById('video-type').value = page.contentType;
+        },
+        clear: function () {
+            document.getElementById('video-title').value = '';
+            document.getElementById('video-src').value = '';
+            document.getElementById('video-type').value = '';
+        },
+        save: function () {
+            dataStore.setPage({
+                id: createId(),
+                type: "video",
+                title: this.getTitle(),
+                src: this.getSrc(),
+                contentType: this.getContentType()
+            });
+            this.clear();
+        },
+        validate: function () {
+            var success = true;
+            if (!validateRequired('video-title')) {
+                success = false;
+            }
+            if (!validateRequired('video-src')) {
+                success = false;
+            }
+            if (!validateRequired('video-type')) {
+                success = false;
+            }
+            return success;
+        },
+        getTitle: function () {
+            return document.getElementById('video-title').value.trim();
+        },
+        getSrc: function () {
+            return document.getElementById('video-src').value.trim();
+        },
+        getContentType: function () {
+            return document.getElementById('video-type').value.trim();
+        },
+    };
+
+    var Image = {
+        form: function () {
+            var html = '<div class="image-form">';
+            html += '<h3>Image</h3>';
+            html += '<div class="form-group">';
+            html += '<label for="image-title">Title</label><br>';
+            html += '<input type="text" id="image-title" class="form-control">';
+            html += '<span class="form-error" id="image-title-error"></span>';
+            html += '</div>';
+            html += '<div class="form-group">';
+            html += '<label for="image-src">Image URL</label><br>';
+            html += '<input type="text" id="image-src" class="form-control">';
+            html += '<span class="form-error" id="image-src-error"></span>';
+            html += '</div>';
+            return html;
+        },
+        update: function (page) {
+            document.getElementById('image-title').value = page.title;
+            document.getElementById('image-src').value = page.src;
+        },
+        clear: function () {
+            document.getElementById('image-title').value = '';
+            document.getElementById('image-src').value = '';
+        },
+        save: function () {
+            dataStore.setPage({
+                id: createId(),
+                type: "image",
+                title: this.getTitle(),
+                src: this.getSrc()
+            });
+            this.clear();
+        },
+        validate: function () {
+            var success = true;
+            if (!validateRequired('image-title')) {
+                success = false;
+            }
+            if (!validateRequired('image-src')) {
+                success = false;
+            }
+            return success;
+        },
+        getTitle: function () {
+            return document.getElementById('image-title').value.trim();
+        },
+        getSrc: function () {
+            return document.getElementById('image-src').value.trim();
+        }
+    };
+
+    var Quiz = {
+        form: function () {
+            var html = '<div class="quiz-form">';
+            html += '<h3>Quiz</h3>';
+            html += '<div class="form-group">';
+            html += '<label for="image-title">Title</label><br>';
+            html += '<input type="text" id="image-title" class="form-control">';
+            html += '<span class="form-error" id="image-title-error"></span>';
+            html += '</div>';
+            html += '<div class="form-group">';
+            html += '<label for="image-src">Image URL</label><br>';
+            html += '<input type="text" id="image-src" class="form-control">';
+            html += '<span class="form-error" id="image-src-error"></span>';
+            html += '</div>';
+            return html;
+        },
+        update: function (page) {
+            document.getElementById('image-title').value = page.title;
+            document.getElementById('image-src').value = page.src;
+        },
+        clear: function () {
+            document.getElementById('image-title').value = '';
+            document.getElementById('image-src').value = '';
+        },
+        save: function () {
+            dataStore.setPage({
+                id: createId(),
+                type: "image",
+                title: this.getTitle(),
+                src: this.getSrc()
+            });
+            this.clear();
+        },
+        validate: function () {
+            var success = true;
+            if (!validateRequired('quiz-title')) {
+                success = false;
+            }
+            return success;
+        },
+        getTitle: function () {
+            return document.getElementById('image-title').value.trim();
+        },
+        getSrc: function () {
+            return document.getElementById('image-src').value.trim();
+        }
+    };
+
     // Displays the current admin manager state.
     var display = function () {
         if (loginManager.isLoggedIn()) {
@@ -169,8 +344,7 @@ var adminManager = (function () {
                 currentPage = getPage('post');;
             }
 
-            var pages = dataStore.findPages();
-            var html = generateHtml(pages);
+            var html = generateHtml();
             content.render(html);
         } else {
             content.render('<p>You must be logged in to view this page</p>');
@@ -182,21 +356,36 @@ var adminManager = (function () {
         return crypto.getRandomValues(new Uint32Array(1)).join('');
     }
 
+    var getPageSelectHtml = function() {
+        var html = '<select id="page-select" onchange="adminManager.pageChange()" class="form-control">';
+        html += '<option value="create">Create new page</option>';
+        html += '<option disabled>----</option>';
+
+        var pages = dataStore.findPages();
+        pages.forEach(function (page) {
+            html += '<option value="' + page.id + '">' + page.title + ' (' + page.type + ')</option>';
+        });
+
+        html += '</select>';
+        return html;
+    }
+
+    var updatePageSelectControl = function() {
+        var html = getPageSelectHtml();
+        document.getElementById('page-select-control').innerHTML = html;
+    }
+
     // Generates HTML to display the admin form
-    var generateHtml = function (pages) {
+    var generateHtml = function () {
         var html = '<form id="admin-form">';
         html += '<h2>Manage Pages</h2>';
 
         html += '<hr>';
         html += '<div class="form-group">';
         html += '<label for="page-select">Select page to manage</label><br>';
-        html += '<select id="page-select" onchange="adminManager.pageChange()" class="form-control">';
-        html += '<option value="create">Create new page</option>';
-        html += '<option disabled>----</option>';
-        pages.forEach(function (page) {
-            html += '<option value="' + page.id + '">' + page.title + ' (' + page.type + ')</option>';
-        });
-        html += '</select>';
+        html += '<div id="page-select-control">';
+        html += getPageSelectHtml();
+        html += '</div>';
         html += '<hr>';
         html += '</div>';
 
@@ -204,6 +393,7 @@ var adminManager = (function () {
         html += '<label for="form-select">Select the type of page</label><br>';
         html += '<select id="form-select" onchange="adminManager.formChange()" class="form-control">';
         html += '<option value="post">Post</option>';
+        html += '<option value="image">Image</option>';
         html += '<option value="slideshow">Slideshow</option>';
         html += '<option value="quiz">Quiz</option>';
         html += '<option value="video">Video</option>';
@@ -231,10 +421,12 @@ var adminManager = (function () {
                 return Post;
             case 'slideshow':
                 return Slideshow;
-            // case 'quiz':
-            //     return { form: quizForm, save: quizSave };
-            // case 'video':
-            //     return { form: videoForm, save: videoSave };
+            case 'quiz':
+                return Quiz;
+            case 'video':
+                return Video;
+            case 'image':
+                return Image;
         }
         return null;
     }
@@ -246,13 +438,13 @@ var adminManager = (function () {
     }
 
     // Changes to show form for page type
-    var performFormChange = function(pageType) {
+    var performFormChange = function (pageType) {
         var page = getPage(pageType);
         if (page == null) {
             throw 'Form type not found';
         } else {
-            currentPage = page; // Set for whole module.
-            var html = currentPage.form(); // Update this form.
+            currentPage = page;
+            var html = currentPage.form();
             document.getElementById('form-content').innerHTML = html;
             message('');
         }
@@ -269,7 +461,7 @@ var adminManager = (function () {
             currentPage = getPage('post');
             document.getElementById('form-content').innerHTML = currentPage.form();
         } else {
-            // disenable select input
+            // disable select input
             document.getElementById('form-select-box').style.display = 'none';
             document.getElementById('deleteButton').style.display = 'inline';
             var page = dataStore.findPage(pageId);
@@ -286,7 +478,7 @@ var adminManager = (function () {
     }
 
     // Clears all form error messages.
-    var clearFormMessages = function () {
+    var clearFormErrors = function () {
         var children = document.getElementsByClassName('form-error');
         for (var child in children) {
             children[child].innerHTML = '';
@@ -296,14 +488,16 @@ var adminManager = (function () {
     // Called to save the current page state.
     var save = function () {
         if (currentPage == null) {
-            throw 'Current state not set';
+            throw 'Current page not set';
         } else {
             // Save if page valid.
-            clearFormMessages();
+            clearFormErrors();
             if (currentPage.validate()) {
                 currentPage.save();
                 message('Page saved!');
+                
                 app.refreshMenu(); // Tell app to redraw main menu.
+                updatePageSelectControl();
             }
         }
     }
@@ -311,16 +505,17 @@ var adminManager = (function () {
     // Removes page from local storage.
     var deletePage = function () {
         if (currentPage == null) {
-            throw 'Current state not set';
+            throw 'Current page not set';
         } else {
             var pageId = document.getElementById('page-select').value;
             dataStore.removePage(pageId);
-            
+
             document.getElementById('page-select').value = 'create';
             performFormChange('post');
             document.getElementById('form-select-box').style.display = 'block';
 
             app.refreshMenu();
+            updatePageSelectControl();
         }
     }
 
