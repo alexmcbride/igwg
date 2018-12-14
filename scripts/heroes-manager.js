c53bddd92d039c0a38f13b5548a5a39421071fd4
Alex McBride <xandermcbride@gmail.com>
2018-12-14 12:40:20 +0000

    Tweak

--- scripts/heroes-manager.js
+++ scripts/heroes-manager.js
@@ -1,4 +1,4 @@
-// Module to encapsulate heroes paginated list.
+// Module to encapsulate heroes master/detail list.
 var heroesManager = (function () {
     var generateHtml = function (data, person) {
         var html = '';
@@ -8,30 +8,28 @@ var heroesManager = (function () {
         html += '<div class="col" id="master">';
         html += '<ul>';
         data.persons.forEach(function (person, index) {
-            var url = urlHelper.generateHash(data.type, data.id, index);
-            html += '<li><a href="#' + url + '">' + person.title + '</a></li>';
+            var hash = urlHelper.generateHash(data.type, data.id, index);
+            html += '<li><a href="#' + hash + '">' + person.title + '</a></li>';
         });
         html += '</ul>';
         html += '</div>';
         html += '<div class="col" id="detail">';
-        if (person !== undefined) {
-            html += '<h3>' + person.title + '</h3>';
-            html += '<ul>';
-            html += '<li>Date: ' + person.publDate + '</li>';
-            html += '<li>Descript: ' + person.descript + '</li>';
-            html += '<li>History: ' + person.history + '</li>';
-            html += '<li>Subject: ' + person.subject + '</li>';
-            html += '<li>Source: ' + person.source + '</li>';         
-            html += '</ul>';
-        }
+        html += '<h3>' + person.title + '</h3>';
+        html += '<ul>';
+        html += '<li>Date: ' + person.publDate + '</li>';
+        html += '<li>Descript: ' + person.descript + '</li>';
+        html += '<li>History: ' + person.history + '</li>';
+        html += '<li>Subject: ' + person.subject + '</li>';
+        html += '<li>Source: ' + person.source + '</li>';         
+        html += '</ul>';
         html += '</div>';
         html += '</div>';
         return html;
     };
 
     var display = function (data) {
-        var personIndex = urlHelper.index();
-        var person = data.persons[personIndex];
+        var index = urlHelper.index();
+        var person = data.persons[index];
         var html = generateHtml(data, person);
         content.render(html);
     };
